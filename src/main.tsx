f9ba4a1ed56e574b742e0ec90a9ba923bb7403d3
SifatU360 <sifatullah30.info@gmail.com>
2025-02-22 21:07:08 +0600

    Redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Provider } from "react-redux";
+import { Store } from './Redux/store';
+import App from './App';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={Store}>   
+        <App/>  
+    </Provider>
   </StrictMode>,
 )
